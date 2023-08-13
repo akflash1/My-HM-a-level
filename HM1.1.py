a8c6e1afda9c48fccad1ef31063be177b64dad97
Yaroslav <yrmn10@gmail.com>
2023-08-13 19:45:10 +0300

    fix bags

---
+++ HM1.1.py
@@ -0,0 +1,40 @@
+class Phone:
+    def __init__(self):
+        self.number = ''
+        self._incoming_calls = 0
+
+    def __str__(self):
+       return f'Number: {self.number}, _incoming_calls: {self._incoming_calls}'
+
+    def set_number(self, number):
+        self.number = number
+
+    def _get_call(self):
+        return self._incoming_calls
+
+    def get_receive(self):
+        self._incoming_calls += 1
+
+    def save_calls_to_file(self, file_name):
+        with open(file_name, 'a') as file:
+            file.write("Total calls received: " + str(self._incoming_calls))
+
+phone1 = Phone()
+phone1.set_number('0662952936')
+phone1.get_receive()
+
+phone2 = Phone()
+phone2.set_number('0953526875')
+phone2.get_receive()
+phone2.get_receive()
+
+phone3 = Phone()
+phone3.set_number('0987419652')
+phone3.get_receive()
+phone3.get_receive()
+phone3.get_receive()
+
+for phone in [phone1, phone2, phone3]:
+    phone.save_calls_to_file('calls_info.txt')
+
+print("Total calls received:", phone1.get_total_calls())
---
+++ HM1.2.py
@@ -0,0 +1,90 @@
+class ChessPiece:
+    WHITE = 'white'
+    BLACK = 'black'
+
+    def __init__(self, color, position):
+        self.color = color
+        self.position = position
+
+    def __str__(self):
+        return f'Color: {self.color}, Position: {self.position}'
+
+    def change_color(self):
+        if self.color == WHITE:
+            self.color = BLACK
+        else:
+            self.color = WHITE
+
+    def is_within_board(self, x, y):
+        return 0 <= x < 8 and 0 <= y < 8
+
+    def can_move(self, new_position):
+        x, y = new_position
+        return self.is_within_board(x, y)
+
+class Pawn(ChessPiece):
+    def can_move(self, new_position):
+        x, y = new_position
+        dx = abs(x - self.position[0])
+        dy = abs(y - self.position[1])
+        if dx == 1 and dy == 0:
+            return True
+        return False
+
+class Knight(ChessPiece):
+    def can_move(self, new_position):
+        x, y = new_position
+        dx = abs(x - self.position[0])
+        dy = abs(y - self.position[1])
+        return (dx == 2 and dy == 1) or (dx == 1 and dy == 2)
+
+class Bishop(ChessPiece):
+    def can_move(self, new_position):
+        x, y = new_position
+        dx = abs(x - self.position[0])
+        dy = abs(y - self.position[1])
+        return dx == dy
+
+class Rook(ChessPiece):
+    def can_move(self, new_position):
+        x, y = new_position
+        return x == self.position[0] or y == self.position[1]
+
+class Queen(ChessPiece):
+    def can_move(self, new_position):
+        x, y = new_position
+        dx = abs(x - self.position[0])
+        dy = abs(y - self.position[1])
+        return dx == dy or x == self.position[0] or y == self.position[1]
+
+class King(ChessPiece):
+    def can_move(self, new_position):
+        x, y = new_position
+        dx = abs(x - self.position[0])
+        dy = abs(y - self.position[1])
+        return dx <= 1 and dy <= 1
+
+def get_possible_moves(pieces, new_position):
+    valid_pieces = []
+    for piece in pieces:
+        if piece.can_move(new_position):
+            valid_pieces.append(piece)
+    return valid_pieces
+
+pawn = Pawn("white", (1, 2))
+knight = Knight("black", (3, 2))
+bishop = Bishop("white", (2, 3))
+rook = Rook("black", (4, 4))
+queen = Queen("white", (5, 5))
+king = King("black", (6, 6))
+
+chess_pieces = [pawn, knight, bishop, rook, queen, king]
+
+new_position = (3, 3)
+
+possible_moves = get_possible_moves(chess_pieces, new_position)
+
+for piece in possible_moves:
+    print(f"{piece.color} {piece.__class__.__name__} can move to {new_position}.")
+
+    
