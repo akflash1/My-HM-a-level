f2a011269894f825c8ca6ccef288cd52cbacbd99
akflash1 <136748557+akflash1@users.noreply.github.com>
2023-08-29 08:15:48 +0300

    test_employee.py

--- OOP/tests/test_employee.py
+++ OOP/tests/test_employee.py
@@ -1,9 +1,34 @@
-from unittest import TestCase
+import unittest
+from datetime import datetime
+from unittest.mock import patch
+from HM2_2 import Employee
 
+class TestEmployee(unittest.TestCase):
 
-class TestSum(TestCase):
-    def test_sum(self):
-        self.assertEqual(6, sum([0, 2, 3]), "Should be 6")
+    @patch('datetime.datetime')
+    def test_check_salary_weekday(self, mock_datetime):
+        employee = Employee("John", 500, email="john@gmail.com")
+        datetime_now = datetime(2023, 8, 28)
+        mock_datetime.now.return_value = datetime_now
+        self.assertEqual(employee.check_salary(5), "Salary for the transferred number of days: 2500")
 
-    def test_sum_tuple(self):
-        self.assertEqual(6, sum((1, 2, 3)), "Should be 6")
+    @patch('datetime.datetime')
+    def test_check_salary_weekend(self, mock_datetime):
+        employee = Employee("John", 0, email="john@gmail.com")
+        datetime_now = datetime(2023, 8, 26)
+        mock_datetime.now.return_value = datetime_now
+        self.assertEqual(employee.check_salary(5), "Salary for the transferred number of days: 0")
+
+    def test_comparison(self):
+        employee1 = Employee("John", 100, email="john@gmail.com")
+        employee2 = Employee("Alice", 150, email="alice@gmail.com")
+
+        self.assertTrue(employee1 < employee2)
+        self.assertFalse(employee1 > employee2)
+        self.assertTrue(employee1 <= employee2)
+        self.assertFalse(employee1 >= employee2)
+        self.assertFalse(employee1 == employee2)
+        self.assertTrue(employee1 != employee2)
+
+if __name__ == '__main__':
+    unittest.main()
